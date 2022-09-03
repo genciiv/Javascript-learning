8e3ab3a44f2084b79486e2ea1dfca6d2058c949c
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-03 22:59:41 +0200

    Add files via upload

---
+++ Javascript/Data Types 2/datatypes.js
@@ -0,0 +1,7 @@
+
+         console.log(10); // Number
+         console.log(19.20); //Number
+         console.log('text'); //String
+         console.log(true); //Boolean
+         console.log(false); //Boolean
+         console.log(null); // Null
