1d7a92643bd72a66521ce14b36cd8932723ff26d
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-03 23:00:04 +0200

    Add files via upload

---
+++ Javascript/Arithmetic Operators 3/arithmetic.js
@@ -0,0 +1,6 @@
+
+        console.log (3+4); //print 7
+        console.log(5-1); //print 4
+        console.log(4*2); //print 8
+        console.log(9/3); //print 3
+        console.log(10%3); //print 1
