9c2d21b3dfa1ee7cf41c8d71c0c6927c5508e4e0
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-04 12:43:56 +0200

    Update methods.js

---
+++ Javascript/Methods 6/methods.js
@@ -0,0 +1,8 @@
+console.log('hello'.toUppercase()); //prints HELLO
+console.log('Hey'.startsWith('H')); //prints true
+
+console.log('iLoveCoding'.toUpperCase()); // prints ILOVECODING
+
+console.log('        iLoveCoding       '.trim());
+
+console.log('console.log('        iLoveCoding       ');
