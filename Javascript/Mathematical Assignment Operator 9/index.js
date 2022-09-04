fccf4912c9fde41466043f83c167c170f241985d
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-04 15:15:10 +0200

    Update index.js

---
+++ Javascript/Mathematical Assignment Operator 9/index.js
@@ -0,0 +1,38 @@
+ let a = 4;
+ a = a + 1;
+ console.log(a); // Output:5
+
+  let b = 4;
+  b+ = 1;
+  console.log(a); //Output : 5
+
+ let x = 20;
+ x -= 5; // Can be written as x=x-5
+ console.log(x); // Output : 15
+
+ let y = 50;
+ y *= 2; // Can be written as y = y * 2 
+ console.log(y); // Output : 100
+
+ let z = 8;
+ y /= 2; // Can be written as z = z / 2 
+ console.log(y); // Output : 4
+
+
+ //Exercise
+
+ let levelUp = 10;
+ levelUp += 5;
+ console.log(levelUp);
+
+ let powerLevel = 150;
+ powerLevel -= 100;
+ console.log(powerLevel);
+
+ let multiplyMe = 5;
+ multiplyMe *=11;
+ console.log(multiplyMe);
+
+ let quarterMe = 100;
+ quarterMe /= 4;
+ console.log(quarterMe);
