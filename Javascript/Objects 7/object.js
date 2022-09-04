b89067416fa5509338c6cef9afcf5f99ae69473b
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-04 13:07:56 +0200

    Update object.js

---
+++ Javascript/Objects 7/object.js
@@ -0,0 +1,11 @@
+console.log(Math.random()); // prints a random number between 0 and 1
+
+console.log(Math.random()); // prints a number between 0 and 1
+
+console.log(Math.floor(Math.random()*50)); // prints a whole number
+
+console.log(Math.random()*100) // prints a number between 0 and 100
+
+console.log(Math.floor(Math.random()*100)); // prints a whole number between 0 and 100
+
+console.log(Math.ceil(43.8)); // prints 44
