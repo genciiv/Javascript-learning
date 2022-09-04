53c092f8d4df13d0742c1f5568b658aaf9c80d23
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-04 10:55:29 +0200

    Add files via upload

---
+++ Javascript/Strings 4/string.js
@@ -0,0 +1,9 @@
+console.log("hi" + "Ya"); //prints 'hiYa'
+console.log('wo'+'ah');//prints 'woah'
+console.log('i love to'+'code')//prints 'i love to code'
+
+conssole.log('front' + 'space');
+conssole.log('back' + 'space');
+conssole.log('no' + 'space');
+
+console.log('one '+'two '+'three');
