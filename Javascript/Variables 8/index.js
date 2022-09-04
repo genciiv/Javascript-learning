7b5b847f0142bee5f0b913795aa97349af9fbda7
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-04 14:41:17 +0200

    Update index.js

---
+++ Javascript/Variables 8/index.js
@@ -0,0 +1,12 @@
+ var myName = 'Genci';
+ console.log(myName);
+//Output : Genci
+
+ var favoriteFood = 'Pizza';
+ var numOfSlices = 8;
+ console.log(favoriteFood);
+ console.log(numOfSlices);
+
+ const food = 'Pizza';
+ console.log(food);
+ food='Tacos'; 
