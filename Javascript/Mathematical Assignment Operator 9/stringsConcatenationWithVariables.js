660690973a332df4d5676e8468ac5dab07393ee6
G-code <30412971+genciiv@users.noreply.github.com>
2022-09-04 16:25:49 +0200

    Add files via upload

---
+++ Javascript/Mathematical Assignment Operator 9/stringsConcatenationWithVariables.js
@@ -0,0 +1,25 @@
+ let myPet = 'Beny';
+ console.log('I own a pet' + myPet); 
+// Output I own a pet Beny
+
+
+let favoriteAnimal = 'Cat';
+console.log('My favorite animal is' + favoriteAnimal);
+// Output : My favorite animal is Cat
+
+
+const myPet = 'Lucky';
+console.log('I own a pet ${myPet}.');
+// Output : I own a pet Lucky
+
+let myName = 'Genci';
+let myCity = 'Fier';
+console.log('My name is ${myName}. My Favorite city is ${myCity}.');
+
+
+const unkwown1 = 'foo';
+console.log(typeof unkwown1); // output string
+
+const unknown2 = 10;
+console.log(typeof unknown2); // output number
+
